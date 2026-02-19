545b80b027d1f7cf60e4563879afd10044e06810
Cheruiyot222 <cynthiacheruiyot222@gmail.com>
2026-02-19 21:11:36 +0300

    Completed and ready for review

--- Devken.CBC.SchoolManagement.API/Devken.CBC.SchoolManagment.UI/School-System-UI/src/app/Subject/subjects.routes.ts
+++ Devken.CBC.SchoolManagement.API/Devken.CBC.SchoolManagment.UI/School-System-UI/src/app/Subject/subjects.routes.ts
@@ -3,6 +3,7 @@ import { Routes }                from '@angular/router';
 import { SubjectsComponent }     from './list/subjects.component';
 import { SubjectFormComponent }  from './form/subject-form.component';
 import { SubjectDetailsComponent } from './details/subject-details.component';
+import { SubjectEnrollmentComponent } from './subject-enrollment/subject-enrollment.component';
 
 export default [
   {
@@ -11,12 +12,12 @@ export default [
   },
   {
     path: 'create',
-    component: SubjectFormComponent,
+    component: SubjectEnrollmentComponent,
     data: { title: 'Create Subject' },
   },
   {
     path: 'edit/:id',
-    component: SubjectFormComponent,
+    component: SubjectEnrollmentComponent,
     data: { title: 'Edit Subject' },
   },
   {
