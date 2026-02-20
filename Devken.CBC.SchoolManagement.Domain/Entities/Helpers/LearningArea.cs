354859cf98014a514057e61fd06eea32ec4fc8cb
Dicksonkibet <ngetichdickson64@gmail.com>
2026-02-20 09:17:43 +0300

    formative, summative, and competency

---
+++ Devken.CBC.SchoolManagement.Domain/Entities/Helpers/LearningArea.cs
@@ -0,0 +1,24 @@
+﻿using Devken.CBC.SchoolManagement.Domain.Common;
+using Devken.CBC.SchoolManagement.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Devken.CBC.SchoolManagement.Domain.Entities.Helpers
+{
+    public class LearningArea : TenantBaseEntity<Guid>
+    {
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; } = null!;   // e.g. Mathematics
+
+        [MaxLength(20)]
+        public string? Code { get; set; }           // e.g. "MA"
+
+        public CBCLevel Level { get; set; }
+
+        public ICollection<Strand> Strands { get; set; }
+            = new List<Strand>();
+    }
+}
