1700446deaa2bbc4582e5419553cd76e16d0348a
Dicksonkibet <ngetichdickson64@gmail.com>
2026-02-27 11:19:52 +0300

    force redeploy

--- Devken.CBC.SchoolManagement.API/Devken.CBC.SchoolManagment.UI/School-System-UI/src/environments/environment.ts
+++ Devken.CBC.SchoolManagement.API/Devken.CBC.SchoolManagment.UI/School-System-UI/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5167'
+  //apiUrl: 'http://localhost:5167',
+    apiUrl: 'https://localhost:44383/'  // ← your real hosted API URL
 };
