d99a12c7d35aa1dfd859a081fcd6fff6af27903f
Dicksonkibet <ngetichdickson64@gmail.com>
2026-02-27 16:24:31 +0300

    force redeploy

--- Devken.CBC.SchoolManagement.API/Devken.CBC.SchoolManagment.UI/School-System-UI/src/environments/environment.prod.ts
+++ Devken.CBC.SchoolManagement.API/Devken.CBC.SchoolManagment.UI/School-System-UI/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://devken-systems.onrender.com/' // Hosted production API URL
+  apiUrl: 'https://devken-systems.onrender.com' // Hosted production API URL
 };
