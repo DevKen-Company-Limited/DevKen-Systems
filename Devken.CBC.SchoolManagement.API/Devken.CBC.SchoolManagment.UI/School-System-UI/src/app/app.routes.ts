83beec59ba22135c398909624526c5cd4179a007
Dicksonkibet <ngetichdickson64@gmail.com>
2026-02-27 10:00:25 +0300

    all

--- Devken.CBC.SchoolManagement.API/Devken.CBC.SchoolManagment.UI/School-System-UI/src/app/app.routes.ts
+++ Devken.CBC.SchoolManagement.API/Devken.CBC.SchoolManagment.UI/School-System-UI/src/app/app.routes.ts
@@ -141,8 +141,8 @@ export const appRoutes: Route[] = [
             {
                 path: 'finance',
                 children: [
-                    // { path: 'fees', loadChildren: () => import('./finance/fee-item/fee-items.routes').then(m => m.default) },
-                    // { path: 'fee-structure', loadChildren: () => import('./finance/fee-structure/fee-structures.component.routes').then(m => m.default) }
+                    { path: 'fees', loadChildren: () => import('./finance/fee-item/fee-items.routes') },
+                    { path: 'fee-structure', loadChildren: () => import('./finance/fee-structure/fee-structures.component.routes')}
                 ]
             },
 
